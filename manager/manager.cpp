db60550387b71ddb37c20e9095749cd4d3bd602a
rafaelchiafarelli <rafael.chiafarelli@gmail.com>
2023-06-09 14:45:28 -0300

    first version with the crow module

--- manager/manager.cpp
+++ manager/manager.cpp
@@ -1,6 +1,6 @@
 #include "manager.hpp"
 
-
+#include <crow_all.h>
 #include <chrono>
 #include <stdio.h>
 #include "player.hpp"
@@ -11,7 +11,15 @@ int main(int argc, char** argv )
 {
     
     Player P("./test_player.json");
+    crow::SimpleApp app;
 
+    CROW_ROUTE(app, "/")
+    ([&]() {
+        P.OnDemand("nome");
+        return "Hello world!";
+    });
+
+    app.port(18080).run();
     P.Start();
     int count =0;
     while(count < 100)
