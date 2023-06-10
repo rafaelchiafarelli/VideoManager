3a09241b383d70e1d77edd4f36c3cf0b4e6166bc
rafaelchiafarelli <rafael.chiafarelli@gmail.com>
2023-06-10 12:04:34 -0300

    fixing OnDemand last image

--- player/player.cpp
+++ player/player.cpp
@@ -105,26 +105,13 @@ void Player::work_func(int w, int h, int step){
                     {
                         if(sequence.second.cur_repeat >= sequence.second.repeat)
                         {
-                            if(sequence.second.has_last == false)
+                            if(sequence.second.has_last == true)
                             {
-                                if(sequence.second.last_one.empty())
-                                {
-                                    sequence.second.cur_index = 0;
-                                    sequence.second.cur_repeat = 0;
-                                    {
-                                        std::lock_guard<std::mutex> lock(mu);
-                                        OnDemandListName.erase(it);
-                                    }
-                                }
-                                else
-                                {
-                                    //just show the last one
-                                    glue(sequence.second.last_image,screen_img,sequence.second.region);
-                                }
+                                //just show the last one
+                                glue(sequence.second.last_image,screen_img,sequence.second.region);
                             }
                             else
                             {
-
                                 sequence.second.cur_index = 0;
                                 sequence.second.cur_repeat = 0;
                                 {
@@ -162,8 +149,16 @@ Player::Player(std::string config_file)
         std::cout<<"configuration loaded -- loading images"<<std::endl;
         for(auto &sequence: c.sequences)
         {
-            
             std::cout<<"sequence:"<<sequence.second.tostring()<<std::endl;
+            if(sequence.second.x > c.app.window_x ||
+                sequence.second.y > c.app.window_y ||
+                sequence.second.x+sequence.second.w > c.app.window_x ||
+                sequence.second.y+sequence.second.h > c.app.window_y)
+                {
+                    std::cout<<"error: wrong size."<<sequence.second.names.size()<<" images:"<<sequence.second.images.size()<<std::endl;
+                    continue;                    
+                }
+            
             if(sequence.second.names.size() != sequence.second.images.size())
             {
                 std::cout<<"error: wrong size names:"<<sequence.second.names.size()<<" images:"<<sequence.second.images.size()<<std::endl;
@@ -177,6 +172,7 @@ Player::Player(std::string config_file)
                 if(img_f.good())
                 {
                     cv::Mat tmp =imread(f_name, cv::IMREAD_UNCHANGED);
+                    cv::resize(tmp, tmp, cv::Size(sequence.second.w, sequence.second.h), cv::INTER_LINEAR);
                     tmp.copyTo(sequence.second.images[i]);
                 }
                 else
@@ -192,6 +188,7 @@ Player::Player(std::string config_file)
                 if(img_f.good())
                 {
                     cv::Mat tmp =imread(sequence.second.last_one, cv::IMREAD_UNCHANGED);
+                    cv::resize(tmp, tmp, cv::Size(sequence.second.w, sequence.second.h), cv::INTER_LINEAR);
                     tmp.copyTo(sequence.second.last_image);
                 }
             }
@@ -234,18 +231,14 @@ void Player::transparency(cv::Mat img)
  */
 void Player::glue(cv::Mat src, cv::Mat dst, cv::Rect region)
 {
-    cv::Mat sized;
-    cv::resize(src, sized, cv::Size(region.width, region.height), cv::INTER_LINEAR);
     std::vector<cv::Mat> rgbLayer;
     cv::split(src, rgbLayer); // seperate channels
-
-    for(int i = 0; i < sized.rows; i++)
+    for(int i = 0; i < src.rows; i++)
     {
-        for(int j = 0; j < sized.cols; j++)
+        for(int j = 0; j < src.cols; j++)
         {
-            cv::Vec4b & pixel_src = sized.at<cv::Vec4b>(i, j);
+            cv::Vec4b & pixel_src = src.at<cv::Vec4b>(i, j);
             cv::Vec3b & pixel_dst = dst.at<cv::Vec3b>(i + region.x, j + region.y);
-            
             if(pixel_src[3] != 0)
             {
                 pixel_dst[0] = pixel_src[0]*(1-1/pixel_src[3]) + pixel_dst[0]*(1/pixel_src[3]);
@@ -254,5 +247,4 @@ void Player::glue(cv::Mat src, cv::Mat dst, cv::Rect region)
             }
         }
     }
-
 }
